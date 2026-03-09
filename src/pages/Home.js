daddd7383921d6a7c5b29898f83d14e9d2af9f62
Aditya_Chauhan_1109 <chauhanaditya1109@gmail.com>
2026-03-09 11:02:27 +0530

    Uncomment 'Read now' link in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,8 @@ function Home(){
           <div className="content">
             <div style={{color: 'white', fontWeight: 900, fontSize: '4rem'}}>Experience the Gita </div>
             <div style={{color: '#fedf89', fontWeight: 900, fontSize: '4rem'}}>Anywhere, Anytime</div>
-            // <Link to={"/chapter/1"} className="btn btn-light">Read now</Link>
+            <Link to={"/chapter/1"} className="btn btn-light">Read now</Link>
+            <h1>Hello</h1>
           </div>
         </div>
       </section>
